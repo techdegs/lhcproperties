5480b565ccb36edeb7e4f4aedff8f2198dd441ee
Samuel Osei Adu <asamuel355@yahoo.com>
2023-11-06 14:29:06 +0000

    update

--- app/page.tsx
+++ app/page.tsx
@@ -7,6 +7,7 @@ import Image from "next/image";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import Hero from "@/components/Hero";
 import Feature from "@/components/Feature";
+import Product from "@/components/Product";
 
 export default function Home() {
   return (
@@ -14,8 +15,10 @@ export default function Home() {
       <Hero />
       <Feature />
       {/* md:px-16 lg:px-24 px-8 w-full */}
-      <div className="md:px-16 lg:px-24 px-8 relative flex w-full -mr-5 pt-5 max-md:max-w-full max-md:pb-24 max-md:px-5 bg-white">
-
+      <div className="md:px-16 lg:px-24 lg:pb-24 px-8 relative flex w-full -mr-5 pt-5 max-md:max-w-full max-md:pb-24 max-md:px-5 bg-white">
+        <div className="flex flex-wrap gap-6">
+          <Product />
+        </div>
       </div>
     </>
   );
---
+++ components/Product.tsx
@@ -0,0 +1,61 @@
+import { BedDouble, LandPlot, Bath, CheckSquare, Heart } from "lucide-react";
+import Image from 'next/image'
+import Link from 'next/link'
+
+const Product = () => {
+  return (
+    <div className="flex flex-col border border-cyan-100 max-w-[580px] rounded-md">
+      <div className="relative rounded-md">
+        <Image
+          src="/images/house.png"
+          width={340}
+          height={140}
+          className="w-full"
+          alt="product"
+        />
+        <button className="absolute text-white bg-indigo-950 px-2 py-1 top-2 right-2">
+          Rent
+        </button>
+      </div>
+
+      <div className="bg-white p-4">
+        <div className="flex-col gap-4">
+          <div>
+            <h4 className="text-2xl font-bold text-indigo-950">
+              GHS 1,500 <span className="text-sm text-indigo-950">/month</span>
+            </h4>
+            <Link className="text-indigo-950" href={"/"}>
+              Beverly Springfield
+            </Link>
+            <p className="text-indigo-950">Main Street. Spintex Road, Accra.</p>
+          </div>
+          <div className="flex gap-2 justify-between mt-5">
+            <div className="flex items-center gap-1">
+              <BedDouble color="darkblue" size={25} />
+              <span className="text-indigo-950 text-sm">4 Bedroom</span>
+            </div>
+            <div className="flex items-center gap-1">
+              <Bath size={25} color="darkblue" />
+              <span className="text-indigo-950 text-sm">3 Bath</span>
+            </div>
+            <div className="flex items-center gap-1">
+              <LandPlot size={25} color="darkblue" />
+              <span className="text-indigo-950 text-sm">2497 sqft</span>
+            </div>
+          </div>
+          <div className="border-b border-b-gray-500 my-2" />
+          <div className="flex justify-end gap-6">
+            <Link href="/">
+              <CheckSquare size={30} color="darkblue" />
+            </Link>
+            <button>
+              <Heart size={30} color="red" />
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Product
